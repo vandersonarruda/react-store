f89d53b994bec021775a08de8d14a515431fecc9
Vanderson Arruda <contato@vanderson.com.br>
2023-10-18 10:48:09 -0300

    chore: fix table name of product and refactoring auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,10 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@auth/prisma-adapter'
-import { db } from '@/lib/prisma'
+import { prismaClient } from '@/lib/prisma'
 
 export const authOptions = {
-  adapter: PrismaAdapter(db),
+  adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
