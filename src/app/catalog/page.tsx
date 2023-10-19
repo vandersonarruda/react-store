04933e7dc72fb42aba48b9752cf31139b0ab9959
Vanderson Arruda <contato@vanderson.com.br>
2023-10-18 21:46:00 -0300

    chore: add category-item-gradient to tailwind config

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,5 +1,28 @@
-const CatalogPage = () => {
-  return <h1>catalog</h1>
+import { Badge } from '@/components/ui/badge'
+import { ShapesIcon } from 'lucide-react'
+import CategoryItem from './components/category-item'
+import { prismaClient } from '@/lib/prisma'
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({})
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className=" px3 w-fit gap-1 border-2 border-primary py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        <ShapesIcon size={16} />
+        <p>Catálogo</p>
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {categories.map((category) => (
+          <CategoryItem category={category} key={category.id} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default CatalogPage
