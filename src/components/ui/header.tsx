08f3db38ac61a22febd2c77c5e7bdf230fb67d64
Vanderson Arruda <contato@vanderson.com.br>
2023-10-29 23:57:06 -0300

    feat: unsure orders are shown on orders page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -7,6 +7,7 @@ import {
   LogInIcon,
   LogOutIcon,
   MenuIcon,
+  PackageSearchIcon,
   PercentIcon,
   ShoppingCartIcon,
 } from 'lucide-react'
@@ -88,6 +89,18 @@ const Header = () => {
                 >
                   <HomeIcon size={16} />
                   Início
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose asChild>
+              <Link href="/orders">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <PackageSearchIcon size={16} />
+                  Meus Pedidos
                 </Button>
               </Link>
             </SheetClose>
